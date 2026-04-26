6e312b2db5080c532a6a2c6544f10c3e573a0318
Your Name <you@example.com>
2026-04-26 09:10:28 -0700

    chore: sync generated component typings

--- src/components.d.ts
+++ src/components.d.ts
@@ -12,7 +12,6 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     ElAlert: typeof import('element-plus/es')['ElAlert']
-    ElAvatar: typeof import('element-plus/es')['ElAvatar']
     ElBadge: typeof import('element-plus/es')['ElBadge']
     ElBreadcrumb: typeof import('element-plus/es')['ElBreadcrumb']
     ElBreadcrumbItem: typeof import('element-plus/es')['ElBreadcrumbItem']
